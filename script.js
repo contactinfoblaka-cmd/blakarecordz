7e48dc79b4d92c43282727dcb4f24ef3832f59fd
Blaka Recordz <contact.infoblaka@gmail.com>
2026-02-14 06:24:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ document.querySelector('.contact-form').addEventListener('submit', function(even
     
     // On vide les champs du formulaire
     this.reset();
+
+});
+
+document.getElementById('searchBtn').addEventListener('click', function() {
+const searchBar = document.getElementById('searchBar');
+if (searchBar.style.display === "none" || searchBar.style.display === "") {
+searchBar.style.display = "inline-block";
+searchBar.focus();
+} else {
+searchBar.style.display = "none";
+}
 });
